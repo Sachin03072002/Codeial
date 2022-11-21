9471a559018ab2aa85012ab9af33c25ff16e4607
Sachin <sachinmondal720@gmail.com>
2022-11-21 19:59:25 +0530

    manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,10 +1,21 @@
 const { userInfo } = require("os");
 const User = require("../models/user");
-const USer=require('../models/user');
+
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
-    })
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(user){
+                return res.render('user_profile',{
+                    title:"User Profile",
+                    user:user
+                })
+            }
+            return res.redirect('/users/sign-in');
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
+    
 }
 // render the sign up page
 module.exports.signUp=function(req,res){
@@ -19,7 +30,12 @@ module.exports.signIn=function(req,res){
         title: "Codeial | Sign In"
     })
 }
+module.exports.signOut=function(req,res){
+    return res.render('user_sign_in',{
+        title:" Codeial | Sign-out"
+    });
 
+}
 
 //get the signup data
 module.exports.create=function(req,res){
@@ -55,4 +71,38 @@ module.exports.create=function(req,res){
 //sign in and create a session for the user
 module.exports.createSession= function(req,res){
     //todo later
+    //find the user
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log("error in finding user in sign in");
+            return;
+        }
+        //handle user found
+        if(user){
+            //handle mismatching or password which donnt match
+            if(user.password!=req.body.password){
+                return res.redirect('back');
+            }
+
+            //handle session creation
+            res.cookie('user_id',user.id);
+            return res.redirect("/users/profile");
+
+
+        }else{
+            //handle user not found
+            return res.redirect('back');
+        }
+
+    });
+}
+
+
+module.exports.signOut=function(req,res){
+    // if(req.cookies.user_id){
+    //    console.log(req.cookies.user_id);
+    // }else{
+    //     return res.redirect('/users/sign-in');
+    // }
+    console.log(req.cookies.user_id);
 }
