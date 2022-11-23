216569450873ac5efa1ca3cbf1d223eaef565294
Sachin <sachinmondal720@gmail.com>
2022-11-23 11:57:02 +0530

    auth part is added using passport

---
+++ config/passport_layout_stratergy.js
@@ -0,0 +1,64 @@
+const passport=require('passport');
+// const User = require('../models/user');
+
+const LocalStratergy = require('passport-local').Strategy;
+
+const User=require('../models/user');
+//authentication using passport
+passport.use(new LocalStratergy({
+        usernameField: 'email'
+    },
+    function(email,password,done){
+        //find a user and establish the identity
+        User.findOne({email: email},function(err,user){
+            if(err){
+                console.log('Error in finding user --> Passport');
+                return done(err);
+            }
+            if(!user || user.password!=password){
+                console.log('Invalid Username/Password');
+                return done(null,false);
+            }
+            return done(null,user);
+        });
+    }
+));
+
+//serializing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function(user,done){
+    done(null,user.id);
+});
+
+
+
+//deserializing the user from the key in the cookie
+passport.deserializeUser(function(id,done){
+    User.findById(id,function(err,user){
+        if(err){
+            console.log('Error in finding user');
+            return done(err);
+        }
+        return done(null,user);
+    });
+});
+
+
+
+//check if the user is authenticated
+passport.checkAuthentication=function(req,res,next){
+    //if the user is signed in, then pass on the request to the nexst function(controller's action)
+    if(req.isAuthenticated()){
+        return next();
+    }
+    //if the user is not signed in
+    return res.redirect('/users/sign-in');
+
+}
+passport.setAuthenticatedUser=function(req,res,next){
+    if(req.isAuthenticated()){
+        //req.user contains the current signed in user from the seesion cookie and we are just sending this ti the locals for the views
+        res.locals.user=req.user;
+    }
+    next();
+}
+module.exports=passport;
--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db=require('./config/mongoose');
+//used for session cookie
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport_layout_stratergy');
+const { Store } = require('express-session');
+const MongoStore=require('connect-mongo')(session);
+
+
+
 //extract style and scripts from sub pages into the layouts
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
@@ -16,13 +25,39 @@ app.use(express.static('./assets'));
 
 app.use(expressLayouts);
 
-// use express router
-app.use('/', require('./routes'));
+
 
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+//mongo store is used to store the seesion cookie in the db
+app.use(session({
+    name: 'codeial',
+    //todo cheange the secret before deployment in production mode
+    secret: 'blasomething',
+    saveUninitialized:false,//when the user is not looged in
+    resave:false, //when the user is looged in it is not need to alter its information
+    cookie:{
+        //miliseconds
+        maxAge:(1000*60*100)
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection:db,
+            autoRemove:'disabled'
+        }
+        ,function(err){
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
+}));
 
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+// use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if (err){
