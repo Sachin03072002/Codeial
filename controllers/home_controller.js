bbc021184795e4cc0dbe0c24a72c28f7e633da0a
Sachin <sachinmondal720@gmail.com>
2022-11-26 11:44:29 +0530

    making post visible only to the logged in users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,18 @@
+const Post=require('../models/post');
 module.exports.home = function(req, res){
+    // Post.find({},function(err,posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts:posts
+    //     });
+    // });
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:posts
+        });
     
-    return res.render('home', {
-        title: "Home"
     });
 }
 
--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const router= express.Router();
-
+const passport=require('passport');
 
 const postsController=require('../controllers/postcontroller');
 
+//allow only the signed in user to post
 
-router.post('/create',postsController.create);
+router.post('/create',passport.checkAuthentication,postsController.create);
 module.exports=router;
