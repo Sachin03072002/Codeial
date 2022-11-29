68dc0c32815d967f44b4474b5bf01e94820d5dd4
Sachin <sachinmondal720@gmail.com>
2022-11-29 18:18:28 +0530

    update profile page is included

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -21,7 +21,7 @@ module.exports.home = function(req, res){
             return res.render('home', {
                 title: "Codeial | Home",
                 posts:posts,
-                all_users:users
+                all_users: users
             });
         })
         
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,15 +1,27 @@
 const { userInfo } = require("os");
-const User = require("../models/user");
-const USer=require('../models/user');
+const User= require('../models/user');
+
 module.exports.profile = function(req, res){
-    User.findById(req.params.id,function(err,user){
+    // console.log(req.params.id);
+    User.findById(req.params.id, function(err,user){
         return res.render('user_profile', {
             title: 'User Profile',
-            profile_user:user
+            profile_user: user
 
         });
     });
 
+}
+
+
+module.exports.update = function(req,res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err,user){
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('Unauthorised');
+    }
 }
 // render the sign up page
 module.exports.signUp=function(req,res){
